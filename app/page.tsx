8efb66f12bf1276aab36bd23555d27cf09304e0d
romansanchez1994 <romansanchezs@gmail.com>
2026-08-17 09:28:18 +0200

    Implement confirmation for expense deletion
    Add confirmation dialog before deleting an expense.

--- app/page.tsx
+++ app/page.tsx
@@ -124,6 +124,12 @@ export default async function Home() {
                     <button
                         type="submit"
                         className="text-red-600 text-sm"
+                      onClick={(e) => {
+                        if(!confirm("¿Seguro que quieres eliminar este gasto?")
+                           ) {
+                          e.preventDefault();
+                        }
+                        }}
                         >
                           Eliminar
                     </button>
