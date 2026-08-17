6b0eb9d787686ab97c40dfaf89bf8396b43df552
romansanchez1994 <romansanchezs@gmail.com>
2026-08-17 15:26:29 +0200

    Add link to Dashboard in InformesPage

--- app/informes/page.tsx
+++ app/informes/page.tsx
@@ -1,9 +1,16 @@
+import Link from "next/link";
+
 export default function InformesPage() {
   return (
     <main className="p-6 max-w-md mx-auto">
-      <h1 className="text-3xl font-bold">
-        Informe de gastos
+      <Link
+        href="/"
+        className="text-blue-400"
+      >
+        ← Dashboard
+            Informe de gastos
       </h1>
     </main>
   );
 }
+``
