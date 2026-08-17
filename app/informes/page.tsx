5cd58429f071702590ae75d29d8a884b1285e653
romansanchez1994 <romansanchezs@gmail.com>
2026-08-17 15:17:41 +0200

    Create page.tsx

---
+++ app/informes/page.tsx
@@ -0,0 +1,9 @@
+export default function InformesPage() {
+  return (
+    <main className="p-6 max-w-md mx-auto">
+      <h1 className="text-3xl font-bold">
+        Informe de gastos
+      </h1>
+    </main>
+  );
+}
