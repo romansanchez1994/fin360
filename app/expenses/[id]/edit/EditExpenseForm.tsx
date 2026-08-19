3c3985907755d7ca9dcf802ec7e73e79c33ac9b2
romansanchez1994 <romansanchezs@gmail.com>
2026-08-19 14:17:26 +0200

    Update EditExpenseForm.tsx

--- app/expenses/[id]/edit/EditExpenseForm.tsx
+++ app/expenses/[id]/edit/EditExpenseForm.tsx
@@ -1,6 +1,14 @@
 "use client";
 
-export default function EditExpenseForm() {
+export default function EditExpenseForm({
+  expense,
+  categories,
+  subcategories,
+}: {
+  expense: any;
+  categories: any[];
+  subcategories: any[];
+}) {
   return (
     <div>
       Edit Expense Form
