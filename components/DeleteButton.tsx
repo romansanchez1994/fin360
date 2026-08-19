09da3622bd4f0fecc4261044448e2e96e1294e20
romansanchez1994 <romansanchezs@gmail.com>
2026-08-19 16:03:33 +0200

    Update DeleteButton.tsx

--- components/DeleteButton.tsx
+++ components/DeleteButton.tsx
@@ -1,1 +1,21 @@
 "use client";
+
+export default function DeleteButton() {
+  return (
+    <button
+      type="submit"
+      className="text-red-400 text-sm"
+      onClick={(e) => {
+        if (
+          !window.confirm(
+            "¿Seguro que quieres eliminar este gasto?"
+          )
+        ) {
+          e.preventDefault();
+        }
+      }}
+    >
+      🗑 Eliminar
+    </button>
+  );
+}
