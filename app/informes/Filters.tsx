493c2c1ef1f10e860a5ffd8bce6babc7ad6c6bda
romansanchez1994 <romansanchezs@gmail.com>
2026-08-19 09:01:16 +0200

    Add paragraph to Filters component
    Updated Filters component to include a new paragraph element with the text 'Filtros'.

--- app/informes/Filters.tsx
+++ app/informes/Filters.tsx
@@ -2,8 +2,10 @@
 
 export default function Filters() {
   return (
-    <div>
-      Filters
+    <div className="mb-6">
+      <p className="mb-2 text-gray-400">
+        Filtros
+      </p>
     </div>
   );
 }
